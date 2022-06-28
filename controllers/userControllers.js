5add8f9aa84295471cb5dfd9bceb32967a848065
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-28 11:25:45 +1000

    added get user by id

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 const { ObjectId } = require("mongoose").Types;
 const { User, Thought } = require("../models");
 
+// get (find) all the users
 const getAllUsers = async (req, res) => {
 	try {
 		const users = await User.find();
@@ -14,10 +15,31 @@ const getAllUsers = async (req, res) => {
 	} catch (error) {
 		console.log("\n---USER CTRL: GET USERS ERR");
 		console.log(error);
-		res.status(500).json({ message: "error" });
+		res.status(500).json({ message: "Something went wrong!" });
+	}
+};
+
+// get a single user by id
+const getUserById = async (req, res) => {
+	try {
+		const userId = req.params.userId;
+		const user = await User.findOne({ _id: userId })
+			.select("-__v")
+			// populate the thoughts and friends fields
+			.populate([
+				{ path: "thoughts", select: "-__v" },
+				{ path: "friends", select: "-__v" }
+			]);
+
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("\n---USER CTRL: GET USER BY ID ERR");
+		console.log(error);
+		res.status(500).json({ message: "Something went wrong!" });
 	}
 };
 
 module.exports = {
-	getAllUsers
+	getAllUsers,
+	getUserById
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 
 // get the controllers for user
-const { getAllUsers } = require("../../controllers/userControllers");
+const {
+	getAllUsers,
+	getUserById
+} = require("../../controllers/userControllers");
 
 // PATH: /api/users
 router.route("/").get(getAllUsers);
 // .post(addNewUser)
 
 // PATH: /api/users/:userId
-// router.route('/:userId')
-//   .get(getUserById)
+router.route("/:userId").get(getUserById);
 
 module.exports = router;
